c4b0625aee809b4185af382886e22d1e65fb4062
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-06-15 19:40:16 -0400

    fix resizing bug

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -17,6 +17,19 @@ const Score = () => {
         const render = () => {
             if (scoreRef.current === null) return;
             renderAbcjs(music, scoreRef.current.getBoundingClientRect().width);
+            /*
+            Unfortunately, the abcjs.render function is not pure, and modifies the styles of
+            the target element, which is the score in this case. When the scale of the render
+            is less than 1, it adds a width style to the div. This breaks our resize logic.
+            To fix this, we remove the width style if it exists.
+            */
+            const existingStyle = scoreRef.current.getAttribute("style");
+            const widthStart = existingStyle?.indexOf(" width: ");
+            if (existingStyle !== null && widthStart !== undefined && widthStart >= 0) {
+                const widthEnd = existingStyle.indexOf(";", widthStart);
+                const newStyle = existingStyle.slice(0, widthStart) + existingStyle.slice(widthEnd + 1, existingStyle.length);
+                scoreRef.current.setAttribute("style", newStyle);
+            }
             dispatch(highlightCurrentChord());
         };
         window.addEventListener("resize", render);
