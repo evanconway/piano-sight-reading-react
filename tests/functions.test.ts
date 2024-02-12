0c49ff26ad48f08ac3343a3b008f4a60f1513770
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-02-11 23:23:00 -0500

    simplify test readout

--- tests/functions.test.ts
+++ tests/functions.test.ts
@@ -3,37 +3,38 @@ import { getMidiOfPitch } from '../src/music_new/functions';
 import { KeyScaleMidiMap, KeySignatures, PitchClass, ScaleDegrees } from '../src/music_new/models';
 
 describe('getMidiOfPitch', () => {
-    // roots for all keys
-    test('C', () => expect(getMidiOfPitch('C', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(60));
-    test('G', () => expect(getMidiOfPitch('G', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(67));
-    test('D', () => expect(getMidiOfPitch('D', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(62));
-    test('A', () => expect(getMidiOfPitch('A', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(69));
-    test('E', () => expect(getMidiOfPitch('E', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(64));
-    test('B', () => expect(getMidiOfPitch('B', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(71));
-    test('F#', () => expect(getMidiOfPitch('F#', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(66));
-    test('C#', () => expect(getMidiOfPitch('C#', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(61));
-    test('F', () => expect(getMidiOfPitch('F', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(65));
-    test('Bb', () => expect(getMidiOfPitch('Bb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(70));
-    test('Eb', () => expect(getMidiOfPitch('Eb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(63));
-    test('Ab', () => expect(getMidiOfPitch('Ab', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(68));
-    test('Db', () => expect(getMidiOfPitch('Db', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(61));
-    test('Gb', () => expect(getMidiOfPitch('Gb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(66));
-    test('Cb', () => expect(getMidiOfPitch('Cb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(71));
-    test('Am', () => expect(getMidiOfPitch('Am', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(69));
-    test('Em', () => expect(getMidiOfPitch('Em', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(64));
-    test('Bm', () => expect(getMidiOfPitch('Bm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(71));
-    test('F#m', () => expect(getMidiOfPitch('F#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(66));
-    test('C#m', () => expect(getMidiOfPitch('C#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(61));
-    test('G#m', () => expect(getMidiOfPitch('G#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(68));
-    test('D#m', () => expect(getMidiOfPitch('D#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(63));
-    test('A#m', () => expect(getMidiOfPitch('A#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(70));
-    test('Dm', () => expect(getMidiOfPitch('Dm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(62));
-    test('Gm', () => expect(getMidiOfPitch('Gm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(67));
-    test('Cm', () => expect(getMidiOfPitch('Cm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(60));
-    test('Fm', () => expect(getMidiOfPitch('Fm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(65));
-    test('Bbm', () => expect(getMidiOfPitch('Bbm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(70));
-    test('Ebm', () => expect(getMidiOfPitch('Ebm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(63));
-    test('Abm', () => expect(getMidiOfPitch('Abm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(68));
+    test('roots of all keys', () => {
+        expect(getMidiOfPitch('C', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(60);
+        expect(getMidiOfPitch('G', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(67);
+        expect(getMidiOfPitch('D', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(62);
+        expect(getMidiOfPitch('A', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(69);
+        expect(getMidiOfPitch('E', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(64);
+        expect(getMidiOfPitch('B', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(71);
+        expect(getMidiOfPitch('F#', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(66);
+        expect(getMidiOfPitch('C#', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(61);
+        expect(getMidiOfPitch('F', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(65);
+        expect(getMidiOfPitch('Bb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(70);
+        expect(getMidiOfPitch('Eb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(63);
+        expect(getMidiOfPitch('Ab', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(68);
+        expect(getMidiOfPitch('Db', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(61);
+        expect(getMidiOfPitch('Gb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(66);
+        expect(getMidiOfPitch('Cb', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(71);
+        expect(getMidiOfPitch('Am', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(69);
+        expect(getMidiOfPitch('Em', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(64);
+        expect(getMidiOfPitch('Bm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(71);
+        expect(getMidiOfPitch('F#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(66);
+        expect(getMidiOfPitch('C#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(61);
+        expect(getMidiOfPitch('G#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(68);
+        expect(getMidiOfPitch('D#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(63);
+        expect(getMidiOfPitch('A#m', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(70);
+        expect(getMidiOfPitch('Dm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(62);
+        expect(getMidiOfPitch('Gm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(67);
+        expect(getMidiOfPitch('Cm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(60);
+        expect(getMidiOfPitch('Fm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(65);
+        expect(getMidiOfPitch('Bbm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(70);
+        expect(getMidiOfPitch('Ebm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(63);
+        expect(getMidiOfPitch('Abm', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(68);
+    });
 });
 
 describe('Cm testing', () => {
@@ -46,15 +47,17 @@ describe('Cm testing', () => {
 describe('key scale midi maps', () => {
     KeySignatures.forEach(keySignature => {
         const scaleMidiMap = KeyScaleMidiMap.get(keySignature);
-        test(`${keySignature} midi map is defined`, () => expect(scaleMidiMap).toBeDefined());
-        if (scaleMidiMap === undefined) return;
-        const pitchClassesFound = new Set<PitchClass>();
-        ScaleDegrees.forEach(degree => {
-            const mapping = scaleMidiMap.get(degree);
-            test(`${keySignature} scale degree ${degree} mapping exists`, () => expect(mapping).toBeDefined());
-            if (mapping === undefined) return;
-            pitchClassesFound.add(mapping.pitchClass);
+        test(`${keySignature} key scale midi map is valid`, () => {
+            expect(scaleMidiMap).toBeDefined();
+            if (scaleMidiMap === undefined) return;
+            const pitchClassesFound = new Set<PitchClass>();
+            ScaleDegrees.forEach(degree => {
+                const mapping = scaleMidiMap.get(degree);
+                expect(mapping).toBeDefined();
+                if (mapping === undefined) return;
+                pitchClassesFound.add(mapping.pitchClass);
+            });
+            expect(pitchClassesFound.size).toBe(ScaleDegrees.length);
         });
-        test(`${keySignature} all pitch classes mapped`, () => expect(pitchClassesFound.size).toBe(ScaleDegrees.length));
     });
 });
