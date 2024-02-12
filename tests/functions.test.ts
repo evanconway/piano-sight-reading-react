2c698e1510152f427f0dca9b5a248c7947052cf1
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-02-11 21:20:22 -0500

    replace jest with vitest

--- tests/functions.test.ts
+++ tests/functions.test.ts
@@ -1,5 +1,8 @@
-describe('check that tests are working', () => {
-    test('5 equals 5', () => {
-        expect(5).toBe(5);
+import { describe, test, expect } from 'vitest';
+import { getMidiOfPitch } from '../src/music_new/functions';
+
+describe('random music generation', () => {
+    test('getMidiOfPitch', () => {
+        expect(getMidiOfPitch('C', { scaleDegree: 1, register: 4, accidental: 0 })).toBe(60);
     });
 });
